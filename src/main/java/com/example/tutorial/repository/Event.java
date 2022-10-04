2caa543225a7f3bf95f6b9705b427a7115aa678a
laphonthipsavath <la.phonthipsavath@gmail.com>
2022-10-03 21:33:19 -0500

    Started DynamnoDB Data.

---
+++ src/main/java/com/example/tutorial/repository/Event.java
@@ -0,0 +1,14 @@
+package com.example.tutorial.repository;
+
+import lombok.Data;
+
+@Data
+public class Event {
+
+    private String uuid;
+
+    private String question;
+
+    private String response;
+
+}
